caa81b5027734f96d8d96477cd54afbc24ea9fa0
David Kironde <david.kironde@andela.com>
2018-04-09 23:18:24 +0300

    add models

--- server.py
+++ server.py
@@ -5,5 +5,21 @@ from flask_potion import Api, ModelResource
 app = flask(__name__)
 db = SQLALCHEMY(app)
 
+class Book(db.model):
+  id = db.Column(db.Integer, primary=True)
+  title = db.Column(db.string(), nullable=False)
+  year_published = db.Column(Integer)
+
+db.create_all()
+
+class BookResource(ModelResource):
+  class Meta:
+    model = Book
+
+api = Api(app)
+api.add_resource(BookResource)
+
+class
+
 if __name__ == '__main__':
   app.run()
